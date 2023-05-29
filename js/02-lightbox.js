1e1b935242c0591cf29712b9855d0155a8005a4f
Иван-Т0 <vanyataran24@gmail.com>
2023-05-29 14:23:09 +0300

    ok

---
+++ js/02-lightbox.js
@@ -0,0 +1,23 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      `;
+    })
+    .join('');
+}
+
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+});
